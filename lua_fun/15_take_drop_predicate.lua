9fa9ea72d5e874ff89830b9e458bf8227ebcbaaa
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-20 08:37:17 +0200

    take and drop functions with predicates

---
+++ lua_fun/15_take_drop_predicate.lua
@@ -0,0 +1,65 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Lua Fun: knihovna pro zpracování konečných i nekonečných sekvencí v jazyce Lua
+
+]]
+
+
+-- načtení knihovny Lua Fun a současně import symbolů do globálního jmenného prostoru
+require "fun"()
+
+
+-- pomocná funkce pro tisk několika prvků nekonečné sekvence
+function printSequence(sequence)
+    for _, value in iter(sequence) do
+        print(value)
+    end
+end
+
+
+g = range(1, 10)
+print("Original sequence")
+print("---------------------------------------------------")
+printSequence(g)
+
+
+print()
+print("take(function(x) return x <= 5 end, sequence)")
+print("---------------------------------------------------")
+printSequence(take(function(x) return x <= 5 end, g))
+
+
+print()
+print("take(function(x) return true end, sequence)")
+print("---------------------------------------------------")
+printSequence(take(function(x) return true end, g))
+
+
+print()
+print("take(function(x) return nil end, sequence)")
+print("---------------------------------------------------")
+printSequence(take(function(x) return nil end, g))
+
+
+print()
+print("drop(function(x) return x <= 5 end, sequence)")
+print("---------------------------------------------------")
+printSequence(drop(function(x) return x <= 5 end, g))
+
+
+print()
+print("drop(function(x) return true end, sequence)")
+print("---------------------------------------------------")
+printSequence(drop(function(x) return true end, g))
+
+
+print()
+print("drop(function(x) return nil end, sequence)")
+print("---------------------------------------------------")
+printSequence(drop(function(x) return nil end, g))
+
+
+-- finito

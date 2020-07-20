721ca0b40d74da6b0ee2d14f16cbcfec94147054
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-20 08:37:17 +0200

    split function

---
+++ lua_fun/13_split_number.lua
@@ -0,0 +1,56 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Lua Fun: knihovna pro zpracování konečných i nekonečných sekvencí v jazyce Lua
+
+]]
+
+
+-- načtení knihovny Lua Fun a současně import symbolů do globálního jmenného prostoru
+require "fun"()
+
+
+-- pomocná funkce pro tisk několika prvků nekonečné sekvence
+function printSequence(sequence)
+    for _, value in iter(sequence) do
+        print(value)
+    end
+end
+
+
+g = range(1, 10)
+print("Original sequence")
+print("---------------------------------------------------")
+printSequence(g)
+
+
+print()
+print("split(5, sequence)")
+print("---------------------------------------------------")
+s1, s2 = split(5, g)
+printSequence(s1)
+print("-- split --")
+printSequence(s2)
+
+
+print()
+print("split(0, sequence)")
+print("---------------------------------------------------")
+s1, s2 = split(0, g)
+printSequence(s1)
+print("-- split --")
+printSequence(s2)
+
+
+print()
+print("split(100, sequence)")
+print("---------------------------------------------------")
+s1, s2 = split(100, g)
+printSequence(s1)
+print("-- split --")
+printSequence(s2)
+
+
+-- finito

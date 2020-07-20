b8c6915f39170371ab52eac4d15dfad9480a4122
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-20 08:37:17 +0200

    Simpler import

---
+++ lua_fun/03_simpler_install_test.lua
@@ -0,0 +1,19 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Lua Fun: knihovna pro zpracování konečných i nekonečných sekvencí v jazyce Lua
+
+]]
+
+
+-- načtení knihovny Lua Fun a současně import symbolů do globálního jmenného prostoru
+require "fun"()
+
+
+-- pokus o použití funkce z knihovny Lua Fun
+print(range(10))
+
+
+-- finito

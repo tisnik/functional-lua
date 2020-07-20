d27f62a84d131a5d455617b17b6cbcdd294c89bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-20 08:37:17 +0200

    Checking Lua installation

---
+++ lua_fun/01_install_test.lua
@@ -0,0 +1,19 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Lua Fun: knihovna pro zpracování konečných i nekonečných sekvencí v jazyce Lua
+
+]]
+
+
+-- načtení knihovny Lua Fun
+fun = require "fun"
+
+
+-- pokus o použití funkce z knihovny Lua Fun
+print(fun.range(10))
+
+
+-- finito

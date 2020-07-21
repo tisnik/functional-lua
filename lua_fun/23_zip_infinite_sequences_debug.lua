ee643ffc214ee9a774be1054e8ff963e4e1f71f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-21 08:25:39 +0200

    Example #23: debugging usage of zip function and infinite sequence

---
+++ lua_fun/23_zip_infinite_sequences_debug.lua
@@ -0,0 +1,52 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Lua Fun: knihovna pro zpracování konečných i nekonečných sekvencí v jazyce Lua
+
+]]
+
+
+-- načtení knihovny Lua Fun a současně import symbolů do globálního jmenného prostoru
+require "fun"()
+
+
+-- pomocná funkce pro tisk několika prvků nekonečné sekvence
+function printZippedSequence(sequence, n)
+    i = 0
+    for index, a, b, c in iter(sequence) do
+        i = i + 1
+        if i > n then
+            break
+        end
+        print(index, a, b, c)
+    end
+end
+
+
+-- vlastní generátor
+function my_generator(x)
+    print("(iter)", x)
+    return (x+1)*(x+1)
+end
+
+
+-- inicializovat vlastní generátor
+g1 = tabulate(my_generator)
+
+-- další generátor nekonečné sekvence 
+g2 = xrepeat("*")
+
+-- třetí generátor nekonečné sekvence
+g3 = zeros()
+
+-- vytvoření zipu ze tří sekvencí
+z1 = zip(g1, g2, g3)
+print()
+print("Zipped sequence (first ten items from infinite sequnce)")
+print("-------------------------------------------------------")
+printZippedSequence(z1, 10)
+
+
+-- finito

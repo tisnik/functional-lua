ac8a8b0e1c77b928123816a86ffc742693cb761d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-21 08:26:08 +0200

    Example #25: filter function

---
+++ lua_fun/25_filter.lua
@@ -0,0 +1,57 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Lua Fun: knihovna pro zpracování konečných i nekonečných sekvencí v jazyce Lua
+
+]]
+
+
+-- načtení knihovny Lua Fun a současně import symbolů do globálního jmenného prostoru
+require "fun"()
+
+
+-- pomocná funkce pro tisk prvků konečné sekvence
+function printSequence(sequence)
+    each(print, sequence)
+end
+
+
+-- vlastní generátor
+function my_generator(x)
+    return (x+1)*(x+1)
+end
+
+
+-- inicializovat vlastní generátor
+g = tabulate(my_generator)
+
+
+-- první podsekvence
+print()
+print("take_n(10, sequence)")
+print("----------------------")
+t10 = take_n(10, g)
+printSequence(t10)
+
+
+-- provést filtraci
+f1 = filter(function(x) return x % 2 == 0 end, g)
+print()
+print("even results")
+print("----------------------")
+t5 = take_n(5, f1)
+printSequence(t5)
+
+
+-- provést filtraci
+f2 = filter(function(x) return not(x % 2 == 0) end, g)
+print()
+print("odd results")
+print("----------------------")
+t5 = take_n(5, f2)
+printSequence(t5)
+
+
+-- finito

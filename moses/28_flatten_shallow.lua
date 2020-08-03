2d920229c09d8be1dc2ac89bf04d72125605957d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-03 10:28:46 +0200

    Example #28: flatten() function with shallow parameter

---
+++ moses/28_flatten_shallow.lua
@@ -0,0 +1,73 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Podpora funkcionálního programovaní v jazyku Lua s využitím knihovny Moses
+
+]]
+
+
+-- načtení knihovny Moses a současně import symbolů do globálního jmenného prostoru
+M = require "moses"
+M.import()
+
+
+-- získaní textové podoby obsahu pole obsahujícího další vnořená pole
+function nestedArray(array)
+    if type(array) == 'table' then
+        -- použít LISPovské s-výrazy
+        local s = '('
+        for i, v in ipairs(array) do
+            -- oddělovač
+            if i > 1 then
+                s = s .. ' '
+            end
+            -- pole mohou být vnořena
+            s = s .. nestedArray(v)
+        end
+        return s .. ')'
+    else
+        return tostring(array)
+    end
+end
+
+
+-- oddělení obsahu
+function printSeparator()
+    print("--------------------------------------------")
+end
+
+
+
+-- původní pole
+a = {1, 2, 3, {4, 5, 6, {7, 8, 9}, {10, 11}}, 12}
+printSeparator()
+
+-- tisk původního pole
+print("Original array")
+print(nestedArray(a))
+print()
+
+-- tisk zploštělého pole
+print("Flatten array")
+print(nestedArray(flatten(a, true)))
+
+
+
+-- původní pole
+b = {"a", {"b", {"c", {"d", {"e", {"f", {"g"}}}}}}}
+printSeparator()
+
+-- tisk původního pole
+print("Original array")
+print(nestedArray(b))
+print()
+
+-- tisk zploštělého pole
+print("Flatten array")
+print(nestedArray(flatten(b, true)))
+
+
+
+-- finito

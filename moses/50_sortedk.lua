9d4bf74edd1832c6d2a9a3ba3b676259f0b39821
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-03 10:56:19 +0200

    Example #50: Sortedk() function to get items in sorted order

---
+++ moses/50_sortedk.lua
@@ -0,0 +1,16 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Podpora funkcionálního programovaní v jazyku Lua s využitím knihovny Moses
+
+]]
+
+
+-- načtení knihovny Moses a současně import symbolů do globálního jmenného prostoru
+M = require "moses"
+M.import()
+
+
+-- finito

57632f991536eb42081d1b671f46bf4326f9a5de
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-17 09:25:33 +0200

    Example #54: count() function

---
+++ moses/54_count.lua
@@ -0,0 +1,53 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Podpora funkcionálního programovaní v jazyku Lua s využitím knihovny Moses
+
+]]
+
+
+-- načtení knihovny Moses a současně import symbolů do globálního jmenného prostoru
+M = require "moses"
+M.import()
+
+-- tisk obsahu pole
+function printArrayInLine(array)
+    -- test na prázdné pole
+    if #array == 0 then
+        print("*empty*")
+        return
+    end
+    for i, value in ipairs(array) do
+        io.write(value)
+        if i ~= #array then
+            io.write(", ")
+        end
+    end
+    print()
+end
+
+-- oddělení obsahu
+function printSeparator()
+    print("-------------------------------")
+end
+
+-- vytvoření a naplnění pole o sto prvcích
+a={}
+for i = 1, 100 do
+    -- náhodná hodnota v rozsahu 1 až 10
+    a[i] = math.random(10)
+end
+
+-- vytištění původního pole
+printSeparator()
+print("Original array")
+printArrayInLine(a)
+
+for i = 1, 10 do
+    print("count(" .. i .. "): " .. count(a, i))
+end
+
+
+-- finito

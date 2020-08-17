bf685acd098467d05a4dd89750d61ddd9e810b56
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-17 09:25:33 +0200

    Example #62: interpose operation on string values

---
+++ moses/62_interpose_string.lua
@@ -0,0 +1,58 @@
+--[[
+
+  Seriál "Programovací jazyk Lua"
+
+  Demonstrační příklad k článku:
+      Podpora funkcionálního programovaní v jazyku Lua s využitím knihovny Moses
+
+]]
+
+
+-- načtení knihovny Moses a současně import symbolů do globálního jmenného prostoru
+M = require "moses"
+M.import()
+
+-- tisk obsahu pole
+function printArray(array)
+    -- test na prázdné pole
+    if #array == 0 then
+        print("*empty*")
+        return
+    end
+    for i, value in ipairs(array) do
+        print(value)
+    end
+    print()
+end
+
+-- oddělení obsahu
+function printSeparator()
+    print("--------------------------------------------")
+end
+
+-- vytvoření a naplnění pole o dvanácti prvcích
+a={"Leden",
+   "Únor",
+   "Březen",
+   "Duben",
+   "Květen",
+   "Červen",
+   "Červenec",
+   "Srpen",
+   "Září",
+   "Říjen",
+   "Listopad",
+   "Prosinec"}
+
+-- vytištění původního pole
+printSeparator()
+print("Original array")
+printArray(a)
+
+printSeparator()
+print("Interposed array")
+b = interpose(a, "")
+printArray(b)
+
+
+-- finito
